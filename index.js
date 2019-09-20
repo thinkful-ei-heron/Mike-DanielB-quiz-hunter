7baba593e40276b555ea7f939cf9cec5696fac19
Daniel Bright <brahyt@gmail.com>
2019-09-20 08:58:17 -0700

    wireframe start

---
+++ index.js
@@ -0,0 +1,59 @@
+//STORE
+//MODEL = [
+  //Question = {
+    //question: 'What color is the youtube icon?', 
+    //possibleAnswers: ['red', 'green', 'blue', 'purple'],
+    //correctAnswer: 'red',
+    //imageAsset: 'url'
+  //}
+//]
+//
+//Display question one at a time
+
+//Home starting screen
+//Question Screen
+//  -display question and possible answers
+//  -display question number and current score
+//  -render with <form>
+//Answer screen
+//  -Tell user after each question if they were right or wrong
+//  -tell user correct answer
+//  -question # and current score
+//  -next question element
+//End Screen
+//  -tally of questions right and wrong
+//  -element for retake
+//  -
+
+function questionBuilder(){
+  console.log({'questionBuilder'})
+}
+
+function renderQuestion(){
+  console.log({'renderQuestion'})
+}
+
+function renderScore(){
+  console.log({'renderScore'})
+}
+
+function storeAnswer(){
+  console.log({'storeAnser'})
+}
+
+function renderAnswer(){
+  console.log({'renderAnswer'})
+}
+
+function renderEnd(){
+  console.log({'renderEnd'})
+}
+
+function start(){
+  questionBuilder()
+  renderQuestion()
+  renderAnswer()
+  renderEnd()
+}
+
+$(start)
