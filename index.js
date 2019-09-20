9a16e8353c03cd0ed670e34025cb4d36ffe92fba
Daniel Bright <brahyt@gmail.com>
2019-09-20 14:42:11 -0700

    cleaned out comments

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
-/* global STORE */
 'use strict';
-//STORE
-//MODEL = [
-  //{
-    //question: 'What color is the youtube icon?', 
-    //possibleAnswers: ['red', 'green', 'blue', 'purple'],
-    //correctAnswer: 'red',
-    //imageAsset: 'url'
-  //}
-//]
-//
-//Display question one at a time
-
-//Home starting screen
-//Question Screen
-//  -display question and possible answers
-//  -display question number and current score
-//  -render with <form>
-//Answer screen
-//  -Tell user after each question if they were right or wrong
-//  -tell user correct answer
-//  -question # and current score
-//  -next question element
-//End Screen
-//  -tally of questions right and wrong
-//  -element for retake
-//  -
-// STORE = [
-//   {
-//     question: 'What color is the youtube icon?', 
-//     possibleAnswers: ['red', 'green', 'blue', 'purple'],
-//     correctAnswer: 'red',
-//     imageAsset: 'url'
-//   }
-// ]
 function questionBuilder(num){ //build HTML from question
   let q = STORE.questions[num]
   let html = `<form>
