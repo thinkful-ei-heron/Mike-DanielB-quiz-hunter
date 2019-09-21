32c581dd57008d05d3b5668a94790bcef0cbd941
Michael Kirsch <michaelskirsch@gmail.com>
2019-09-20 19:50:32 -0600

    Heavy refactoring, finished up functionality, added test mode.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 'use strict';
 //STORE
 //MODEL = [
-  //{
-    //question: 'What color is the youtube icon?', 
-    //possibleAnswers: ['red', 'green', 'blue', 'purple'],
-    //correctAnswer: 'red',
-    //imageAsset: 'url'
-  //}
+//{
+//question: 'What color is the youtube icon?', 
+//possibleAnswers: ['red', 'green', 'blue', 'purple'],
+//correctAnswer: 'red',
+//imageAsset: 'url'
+//}
 //]
 //
 //Display question one at a time
@@ -34,43 +34,13 @@
 //     imageAsset: 'url'
 //   }
 // ]
-function questionBuilder(num){ //build HTML from question
-  let q = STORE.questions[num]
-  let html = `<form>
-    <fieldset>
-      <li>question: ${q.question}</li>`
-  for (let i = 0; i < q.answers.length; i++){
-    html = html.concat(`  <label class="answer" for="">
-    <input class="radio" type="radio" id="ans${i}" value="${q.answers[i]}" name="ans" required>
-    <span>${q.answers[i]}</span>
-  </label>`);
-  }
-  html = html.concat(`
-    <button id='submit-question'>Submit Button Placeholder</button>
-  </fieldset>
-</form>`);
-  return html;
-}
 
-function renderMainPage(){
-  console.log('renderMainPage')
-  
-  renderQuestion(0); //temp
-  renderScore();
-}
+const testMode = true;
 
-function renderQuestion(num){
-  //console.log('renderQuestion')
-  let html = questionBuilder(num);
-  //console.log(html);
-  $('.card').html(html);
-  $('.js-questionNum').text(`Question ${num + 1} of ${STORE.questions.length}`);
-  renderScore();
-}
-
-function renderScore(){
-  // console.log('renderScore')
-  $('.js-score').text(STORE.score);
+function startPageBuilder(){
+  //doesn't really need to be a function but likely to be big enough that we'll want it out of the way
+  return `<p>WIP start page</p>
+  <button id='start-button'>Begin Quiz</button>`;
 }
 
 function checkAnswer(ans){ //ans is full answer string
@@ -78,27 +48,38 @@ function checkAnswer(ans){ //ans is full answer string
   return ans === q.correctAnswer;
 }
 
-function renderAnswer(bool){
-  let html = answerBuilder(bool);
-  $('.card').html(html);
-}
-
-function answerBuilder(bool){
-  let html = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>'
-  if(bool){
-    html = html.concat('<p>YOU\'RE WINNER</p>');
+function answerBuilder(ans){
+  let html;
+  if(checkAnswer(ans)){
+    html = '<p>YOU\'RE WINNER</p>';
   } else {
-    html = html.concat('<p>FAILURE</p>');
+    html = `<p>FAILURE</p>
+    <p>You answered ${ans} but the correct answer was ${STORE.questions[STORE.currentQuestion].correctAnswer}</p>`;
   }
-  //html = html.concat(`<button id='${STORE.currentQuestion < STORE.questions.length - 1 ? 'next-question' : 'finish-quiz'}'>Submit Button Placeholder</button>`);
-  html = html.concat('<button id=\'answer-page-button\'>Submit Button Placeholder</button>');
+  html = html.concat(`<button id='answer-page-button'>${STORE.currentQuestion + 1 < STORE.questions.length ? 'Next Question' : 'Results'}</button>`);
   return html;
 }
 
-function renderEnd(){
-  console.log('renderEnd')
-  $('.js-questionNum').remove();
-  $('.js-score').remove();
+function questionBuilder(){
+  let q = STORE.questions[STORE.currentQuestion];
+  let html = `<form id='question'>
+    <fieldset>
+      <li>question: ${q.question}</li>`;
+  for (let i = 0; i < q.answers.length; i++){
+    html = html.concat(`  <label class="answer" for="">
+    <input class="radio" type="radio" id="ans${i}" value="${q.answers[i]}" name="ans" required>
+    <span>${q.answers[i]}</span>
+  </label>`);
+  }
+  html = html.concat(`
+    <button id='submit-question'>Submit Question</button>
+    ${testMode ? '<button id=\'test-skip\'>TEST: skip question</button>' : ''}
+  </fieldset>
+</form>`); //TODO: don't forget to remove TEST button
+  return html;
+}
+
+function endPageBuilder(){
   let score = STORE.score;
   let numQuestions = STORE.questions.length;
   let html = `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
@@ -116,48 +97,90 @@ function renderEnd(){
     html = html.concat('Worse than chance would suggest. Ouch.');
   }
 
-  html.concat('button id=\'start-over\'>Start Over</button>')
+  html = html.concat('<button id=\'start-over\'>Start Over</button>');
+  return html;
 }
 
-function handleSubmitQuestionClick(){
-  $('.js-card').on('click', '#submit-question', event => {
-    // console.log(event);
+function render(ans = ''){
+  let html = '<p>something went wrong</p>';
+  const state = STORE.state;
+  if ('end' === state || 'start' === state){
+    $('.js-questionNum').hide();
+    $('.js-score').hide();
+  } else {
+    $('.js-score').show().text(STORE.score);
+    $('.js-questionNum').show().text(`Question ${STORE.currentQuestion + 1} of ${STORE.questions.length}`);
+  }
+  switch(state){
+  case 'start':
+    html = startPageBuilder();
+    break;
+  case 'question':
+    html = questionBuilder();
+    break;
+  case 'answer':
+    html = answerBuilder(ans);
+    break;
+  case 'end':
+    html = endPageBuilder();    
+  }
+
+  $('.card').html(html);
+}
+
+function quizControl(answer = ''){
+  switch(STORE.state){
+  case 'start':
+    STORE.state = 'question';
+    render();
+    break;
+  case 'question':
+    if(checkAnswer(answer)) STORE.score++;
+    STORE.state = 'answer';
+    render(answer);
+    break;
+  case 'answer':
+    ++STORE.currentQuestion < STORE.questions.length //prefix increment because we want the new value for comparison
+      ? STORE.state = 'question' : STORE.state = 'end';
+    render();
+    break;
+  case 'end':
+    STORE.state = 'start';
+    STORE.score = 0;
+    STORE.currentQuestion = 0;
+    render();
+    break;
+  default:
+    // eslint-disable-next-line no-console
+    console.error('invalid state, restarting');
+    STORE.state = 'start';
+    render();
+  }
+}
+
+function handleButtonClick(){
+  $('.js-card').on('click', 'button', event => {
     event.preventDefault();
-    let answer = $('.radio:checked').val();
-    if (answer){ //if nothing is checked answer is undefined, which is falsy
-      let success = checkAnswer(answer);
-      if(success){
-        STORE.score++;
-        renderScore();
+    if (STORE.state === 'question'){
+      if (testMode && event.target.id === 'test-skip'){ //TODO: don't forget to remove
+        quizControl(STORE.questions[STORE.currentQuestion].correctAnswer);
       }
-      renderAnswer(success);}
-  });
-
-}
-function handleAnswerPageClick() {
-  $('.js-card').on('click', '#answer-page-button', event => {
-    event.preventDefault();
-    if(STORE.currentQuestion + 1 < STORE.questions.length){
-      STORE.currentQuestion++;
-      renderQuestion(STORE.currentQuestion);
+      let answer = $('.radio:checked').val();
+      //TODO: required doesn't seem to be working, workaround below
+      if (answer){ //if nothing selected, answer is undefined which is falsy
+        quizControl(answer);
+      } else {
+        document.getElementById('question').reportValidity(); //N.B. jQuery objects do not currently support reportValidity()
+      }
     } else {
-      renderEnd();
-    }
-  });
-}
-function handleStartOverClick() {
-  $('.js-card').on('click', '#answer-page-button', event => {
-    event.preventDefault();
-    renderMainPage();
+      quizControl();
+    } 
   });
 }
 
 function start(){
-  renderMainPage();
-  //event handlers
-  handleSubmitQuestionClick();
-  handleAnswerPageClick();
-  handleRestartClick();
+  render(); //render start page
+  handleButtonClick(); //event handler
 }
 
-$(start)
+$(start);
--- store.js
+++ store.js
@@ -1,32 +1,36 @@
+// defines a store to be used elsewhere, so: 
+/* eslint-disable no-unused-vars */ 
 'use strict';
 const STORE = {
   questions: [  
     { 
-      question: "What monster was the featured monster for the release of Monster Hunter World?",
-      answers: ["Vespoid", "Kelbi", "Nergigante", "Raphinos"],
-      correctAnswer: "Nergigante"
+      question: 'What monster was the featured monster for the release of Monster Hunter World?',
+      answers: ['Vespoid', 'Kelbi', 'Nergigante', 'Raphinos'],
+      correctAnswer: 'Nergigante'
     },
     { 
-      question: "Which monster was part of the Witcher crossover?",
-      answers: ["Ancient Leshen", "Pukei-Pukei", "Tobi-Kadachi", "Lavasoith"],
-      correctAnswer: "Ancient Leshen" 
+      question: 'Which monster was part of the Witcher crossover?',
+      answers: ['Ancient Leshen', 'Pukei-Pukei', 'Tobi-Kadachi', 'Lavasoith'],
+      correctAnswer: 'Ancient Leshen' 
     },
     { 
-      question: "Which brute wyvern is noted for having a huge crown atop its skull?",
-      answers: ["Rathalos", "Barroth", "Lunastra", "Great Girros"],
-      correctAnswer: "Barroth"
+      question: 'Which brute wyvern is noted for having a huge crown atop its skull?',
+      answers: ['Rathalos', 'Barroth', 'Lunastra', 'Great Girros'],
+      correctAnswer: 'Barroth'
     },
     { 
-      question: "Which agile monster resembles a unicorn that can shoot lightning from its horn?",
-      answers: ["Kirin", "Diablos", "Vaal Hazak", "Xeno'jiva"],
-      correctAnswer: "Kirin"
+      question: 'Which agile monster resembles a unicorn that can shoot lightning from its horn?',
+      answers: ['Kirin', 'Diablos', 'Vaal Hazak', 'Xeno\'jiva'],
+      correctAnswer: 'Kirin'
     },
     { 
-      question: "Which monster is covered in a golden mantel?",
-      answers: ["Radobaan", "Kulve Taroth", "Odogaron", "Legiana"],
-      correctAnswer: "Kulve Taroth"
+      question: 'Which monster is covered in a golden mantel?',
+      answers: ['Radobaan', 'Kulve Taroth', 'Odogaron', 'Legiana'],
+      correctAnswer: 'Kulve Taroth'
     }
   ],
   currentQuestion: 0,
-  score: 0
-}
+  score: 0,
+  state: 'start' //valid values: 'start' for the start view, 'question' for a question view, 'answer' for an answer view, 'end' for final view
+  //maybe should use enum?
+};
